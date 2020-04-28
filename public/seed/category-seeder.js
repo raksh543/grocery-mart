a3fb009c5c54f3f32c2fe6f46bff632fa8c1bdf4
raksh543 <rakshitajain777@gmail.com>
2020-04-28 15:06:37 +0530

    changes

---
+++ public/seed/category-seeder.js
@@ -0,0 +1,40 @@
+var Category=require('../models/categories')
+var ProductsSchema=require('../models/product')
+
+var mongoose=require('mongoose')
+
+var Product = mongoose.model("Product", ProductsSchema);
+
+mongoose.connect("mongodb+srv://monchu:monchu@cluster0-dgfgi.mongodb.net/Grocery?retryWrites=true&w=majority");//creating or joining to practice database
+
+Product.find(function (err, docs) {
+    productChunks = [];
+    for (var i = 0; i < docs.length; i++) {
+        productChunks.push(docs);
+    }
+})
+
+var Categories=
+    new Category({
+        titlehead:'Beverages',
+        products:productChunks
+    })
+
+Categories.save()
+
+mongoose.disconnect()
+
+// var done=0;
+
+// for (var i=0;i<beverages.length;i++){
+//     beverages[i].save(function(err,result){
+//         done++;
+//         if(done===beverages.length){
+//             exit();
+//         }
+//     });
+// }
+
+// function exit(){
+//     mongoose.disconnect();
+// }
